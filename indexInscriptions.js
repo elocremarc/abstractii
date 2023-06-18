3c2214e195233f2314662c491b8a5c3f3bc9281d
Eloc <0xeloc@gmail.com>
2023-06-18 02:17:59 -0700

    generic indexscript

--- indexInscriptions.js
+++ indexInscriptions.js
@@ -1,10 +1,15 @@
 const crypto = require('crypto');
 const fs = require('fs');
+const fetch = require('node-fetch');
 
 const explorer = 'https://ordinals.com';
-const abstractiiHash = '1816a5cc285047c1c80462d09177e5cdc53c5e086b9f1a735710755f1b5d72f2';
 
-async function getData(url) {
+async function indexInscriptions(fromGenesisBlockHeight, toGenesisBlockHeight, inscriptionContentHash) {
+  const baseUrl = 'https://api.hiro.so/ordinals/v1/inscriptions';
+  const mimeType = 'text/html';
+
+  const url = `${baseUrl}?from_genesis_block_height=${fromGenesisBlockHeight}&to_genesis_block_height=${toGenesisBlockHeight}&mime_type=${mimeType}`;
+
   let offset = 0;
   let limit = 20;
   let allResults = [];
@@ -25,11 +30,11 @@ async function getData(url) {
   allResults.sort((a, b) => a.timestamp - b.timestamp);
 
   let inscriptions = [];
-  let abstractiiCount = 1;
+  let inscriptionCount = 1;
 
   for (let item of allResults) {
-    if(item.genesis_block_height >= 797536) {
-      continue; // Skip if the block height is not before 783098
+    if (item.genesis_block_height >= toGenesisBlockHeight) {
+      continue;
     }
 
     let inscriptionId = item.id;
@@ -40,14 +45,14 @@ async function getData(url) {
       let text = await res.text();
       let hash = crypto.createHash('sha256').update(text).digest('hex');
 
-      if (hash === abstractiiHash) {
+      if (hash === inscriptionContentHash) {
         inscriptions.push({
           id: inscriptionId,
           meta: {
-            name: `Abstractii Evolved #${abstractiiCount}`
-          }
+            name: `Inscription #${inscriptionCount}`,
+          },
         });
-        abstractiiCount++;
+        inscriptionCount++;
       }
     } catch (error) {
       console.error('Error fetching or processing data:', error);
@@ -64,4 +69,8 @@ async function getData(url) {
   });
 }
 
-getData('https://api.hiro.so/ordinals/v1/inscriptions?from_genesis_block_height=794543&to_genesis_block_height=797536&mime_type=text/html');
+const fromGenesisBlockHeight = 794543;
+const toGenesisBlockHeight = 797536;
+const inscriptionContentHash = '1816a5cc285047c1c80462d09177e5cdc53c5e086b9f1a735710755f1b5d72f2';
+
+indexInscriptions(fromGenesisBlockHeight, toGenesisBlockHeight, inscriptionContentHash);
