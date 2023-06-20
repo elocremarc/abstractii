ad96c490fbba552dc666ff350dffdd22728b7fcb
Eloc <0xeloc@gmail.com>
2023-06-20 13:31:48 -0700

    fixed indexing

--- indexInscriptions.js
+++ indexInscriptions.js
@@ -1,6 +1,5 @@
 const crypto = require('crypto');
 const fs = require('fs');
-const fetch = require('node-fetch');
 
 const explorer = 'https://ordinals.com';
 
@@ -27,6 +26,7 @@ async function indexInscriptions(fromGenesisBlockHeight, toGenesisBlockHeight, i
   let offset = 0;
   let limit = 20;
   let allResults = [];
+  let seen = new Set();
 
   while (true) {
     let response = await fetch(`${url}&limit=${limit}&offset=${offset}`);
@@ -41,7 +41,7 @@ async function indexInscriptions(fromGenesisBlockHeight, toGenesisBlockHeight, i
   }
 
   // Sort by timestamp
-  allResults.sort((a, b) => a.timestamp - b.timestamp);
+  allResults.sort((a, b) => a.number - b.number);
 
   let inscriptions = [];
   let inscriptionCount = 1;
@@ -50,31 +50,45 @@ async function indexInscriptions(fromGenesisBlockHeight, toGenesisBlockHeight, i
     if (item.genesis_block_height >= toGenesisBlockHeight) {
       continue;
     }
-
+  
     let inscriptionId = item.id;
-    let url = `${explorer}/content/${inscriptionId}`;
 
+    if (seen.has(inscriptionId)) {
+      continue;
+    }
+
+    seen.add(inscriptionId);
+  
+    let url = `${explorer}/content/${inscriptionId}`;
+  
     try {
       let res = await fetch(url);
       let text = await res.text();
       let hash = crypto.createHash('sha256').update(text).digest('hex');
-
-      if (hash === inscriptionContentHash) {
+  
+      if (hash === inscriptionContentHash || hash === 'eaa87520ad421aa7bc40c43fd30d0beb8dd490cc4080a8945c86c715e7db2406') {
         const inscriptionName = `${inscriptionNamePrefix} #${inscriptionCount}`;
-        inscriptions.push({
+        const inscription = {
           id: inscriptionId,
           meta: {
             name: inscriptionName,
           },
-        });
+        };
+  
+        // Push the inscription into the array
+        inscriptions.push(inscription);
+  
+        // Log the inscription to console
+        console.log(`Found matching inscription: ${JSON.stringify(inscription)}`);
+  
         inscriptionCount++;
       }
     } catch (error) {
       console.error('Error fetching or processing data:', error);
     }
   }
-
-  // Save to inscriptions.json
+  
+  // Save to inscriptions.json only once, after all processing is done
   fs.writeFile('inscriptions.json', JSON.stringify(inscriptions, null, 2), (err) => {
     if (err) {
       console.error('Error writing file:', err);
@@ -88,6 +102,6 @@ async function indexInscriptions(fromGenesisBlockHeight, toGenesisBlockHeight, i
 const fromGenesisBlockHeight = 794543;
 const toGenesisBlockHeight = 797536;
 const inscriptionContentHash = '1816a5cc285047c1c80462d09177e5cdc53c5e086b9f1a735710755f1b5d72f2';
-const inscriptionNamePrefix = 'Inscription';
+const inscriptionNamePrefix = 'Abstractii Evolved';
 
 indexInscriptions(fromGenesisBlockHeight, toGenesisBlockHeight, inscriptionContentHash, inscriptionNamePrefix);
